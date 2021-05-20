bce6144cf340a54ca591481f85589f7afea3fc2a
Trần Hoàng Vũ (VNCDLL-CTÐTKSAI) <vuth5@vingroup.net>
2021-05-20 11:55:06 +0700

    May20

--- siamese triplet loss/dataset.py
+++ siamese triplet loss/dataset.py
@@ -15,9 +15,29 @@ class SiameseLSTMDataset(Dataset):
         X_right_seq = load_file_npy(config["source"]["X_right_seq.npy"])
 
         Y = load_file_npy(config["source"]["label"])
-        
+
+        positive_index = list(np.where(Y == 1)[0])
+        negative_index = list(np.where(Y == 1)[0])
+
+        X_anchor = X_left_seq[positive_index]
+        self.X_anchor = np.concatenate((X_anchor, X_right_seq[positive_index]), axis=0)
+
+        X_positive = X_right_seq[positive_index]
+        self.X_positive = np.concatenate((X_positive, X_left_seq[positive_index]), axis=0)
+
+        X_negative = X_left_seq[negative_index] # target: use all dataset
+        X_negative = np.concatenate((X_negative, X_right_seq[negative_index]), axis=0) # target: use all dataset
+
+        self.X_negative = X_negative[np.random.choice(X_negative.shape[0], X_anchor.shape[0], replace=False), :]
+
+        assert len(X_anchor) == len(X_positive) == len(X_negative)
 
     def __len__(self):
-
+        return len(self.X_anchor)
     
     def __getitem__(self, index):
+        # Anchor, Positive, Negative
+        Anchor = torch.tensor(self.X_anchor[index], dtype=torch.int32)
+        Positive = torch.tensor(self.X_positive[index], dtype=torch.int32)
+        Negative = torch.tensor(self.X_negative[index], dtype=torch.int32)
+        return Anchor, Positive, Negative
