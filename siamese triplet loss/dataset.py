79f79118d2aa8f2514f0d0b0f98647f0333ad34f
Trần Hoàng Vũ (VNCDLL-CTÐTKSAI) <vuth5@vingroup.net>
2021-05-20 11:14:28 +0700

    May20

--- siamese triplet loss/dataset.py
+++ siamese triplet loss/dataset.py
@@ -1,46 +1,23 @@
+from numpy.lib.npyio import load
 import torch
 from torch.utils.data import Dataset
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from config import config
 
+from config import config
 from utils import load_file_npy
 
 class SiameseLSTMDataset(Dataset):
-    def __init__(self, config, model):
-        print(f"Mode {model}")
+    def __init__(self, config):
         super(SiameseLSTMDataset, self).__init__()
-        if model == "train":
+        X_left_seq = load_file_npy(config["source"]["X_left_seq.npy"])
+        X_right_seq = load_file_npy(config["source"]["X_right_seq.npy"])
 
-            self.X_left = load_file_npy(config["source"]["train"]["sentence_1"])
-            self.X_right = load_file_npy(config["source"]["train"]["sentence_2"])
-
-            self.Y = load_file_npy(config["source"]["train"]["label"])
-
-        elif model == "dev":
-
-            self.X_left = load_file_npy(config["source"]["dev"]["sentence_1"])
-            self.X_right = load_file_npy(config["source"]["dev"]["sentence_2"])
-
-            self.Y = load_file_npy(config["source"]["dev"]["label"])
+        Y = load_file_npy(config["source"]["label"])
         
-        else:
-
-            self.X_left = load_file_npy(config["source"]["test"]["sentence_1"])
-            self.X_right = load_file_npy(config["source"]["test"]["sentence_2"])
-
-            self.Y = load_file_npy(config["source"]["test"]["label"])
-
 
     def __len__(self):
-        return len(self.Y)
+
     
     def __getitem__(self, index):
-
-        sentence_1 = torch.tensor(self.X_left[index], dtype=torch.int32)
-        sentence_2 = torch.tensor(self.X_right[index], dtype=torch.int32)
-
-        label = torch.tensor(self.Y[index], dtype=torch.float)
-        return sentence_1, sentence_2, label
